43d421666a7b61bbb09ab6009de027a59ce0f3ea
RY_2718(Ryo Yoshitani) <ry.3607x5531@gmail.com>
2018-11-21 23:53:17 +0900

    :fire: Add: ファイルを消せるようにしたぞ！
    しかしファイルがあまり消えなかったかなしみ

--- index.js
+++ index.js
@@ -2,15 +2,20 @@
 
 require('dotenv').config();
 
-const httpClient = require('request');
+const request = require('request');
 const fs = require('fs');
+const path = require('path');
 
 const slack = require('./src/slack');
 
 const ts_to_date = new Date(2018, 3, 1, 9, 0, 0, 0);
 let total_size = 0;
 
-httpClient.get(
+if (!fs.existsSync('./logs')) {
+  fs.mkdirSync('./logs');
+}
+
+request.get(
   {
     url: 'https://slack.com/api/files.list',
     qs: {
@@ -21,12 +26,14 @@ httpClient.get(
   },
   (error, response, body) => {
     const json = slack.filter_files(JSON.parse(body), 10 ** 6);
-    fs.writeFileSync(
-      'sample_data/files.json',
-      JSON.stringify(slack.shorten(json))
-    );
+    fs.writeFileSync('./logs/files.json', body);
     json['files'].forEach(file => {
       total_size += file['size'];
+      slack.delete_file(
+        file['id'],
+        path.resolve(__dirname, './logs/success.txt'),
+        path.resolve(__dirname, './logs/error.txt')
+      );
     });
     console.log(total_size);
   }
--- src/slack.js
+++ src/slack.js
@@ -1,3 +1,14 @@
+const fs = require('fs');
+const request = require('request');
+
+/*
+  files.listで受け取ったjsonから消したくないファイルを消す関数
+  条件を変えたいときははfilterに渡している関数とテストを書き換えればよい
+
+  @param {Object} json files.listで受け取ったJSON
+  @param {integer} fileSize fileSize以下の大きさのファイルは消えない
+  @return {Object} json 入力のjsonから消したくないファイルを消したもの
+*/
 const filter_files = (json, fileSize) => {
   json['files'] = json['files'].filter(file => {
     if ('pinned_to' in file || file['size'] < fileSize) {
@@ -8,6 +19,13 @@ const filter_files = (json, fileSize) => {
   return json;
 };
 
+/*
+  files.listで受け取ったjsonから一部だけ抜き出す関数
+  console.logに吐き出すとき見やすい！
+
+  @param {Object} json files.listで受け取ったJSON（にフィルタをかけたもの）
+  @return {Object} json 入力の一部の属性を抜き取ったもの
+*/
 const shorten = json => {
   let result = {
     files: [],
@@ -25,7 +43,39 @@ const shorten = json => {
   return result;
 };
 
+/*
+  消したいファイルのIDを渡すと消すためのHTTPS POSTをしてくれる
+
+  @param file {String} 消したいファイルのID
+  @param success_log {String} 成功したファイルIDを書き溜めるlogのpath
+  @param error_log {String} 失敗したときの情報を書き込むlogのpath
+*/
+const delete_file = (file, success_log, error_log) => {
+  request.post(
+    {
+      url: 'https://slack.com/api/files.delete',
+      form: {
+        token: process.env.TOKEN, // TODO: この部分でprocess.envに依存していて良いのか？
+        file: file['id'],
+      },
+    },
+    error => {
+      if (error) {
+        fs.appendFileSync(
+          error_log,
+          `File ID ${file['id']}の削除に失敗しました\n`
+        );
+        console.error(`File ID ${file['id']}の削除に失敗しました`);
+      } else {
+        fs.appendFileSync(success_log, `${file['id']}\n`);
+        console.log(file['id']);
+      }
+    }
+  );
+};
+
 module.exports = {
   filter_files: filter_files,
   shorten: shorten,
+  delete_file: delete_file,
 };
